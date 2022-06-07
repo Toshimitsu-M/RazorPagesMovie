09acc7dcf93794f493eea051447a389c6bd9e06d
Toshimitsu.M <82960302+Toshimitsu-M@users.noreply.github.com>
2022-06-08 01:00:41 +0900

    Add files via upload

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //  UseInternalServiceProvider(IServiceProvider serviceProvider);
+       　// optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test");
+        //}
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+
+
+    }
+}
